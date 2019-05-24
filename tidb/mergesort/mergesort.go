d465930dd1fb645e5bf72effe0b2939970845957
zouhuan1215 <zouhuan1215@gmail.com>
2019-05-24 10:27:13 +0800

    [mergesort] final version, without pprof snippts

--- tidb/mergesort/mergesort.go
+++ tidb/mergesort/mergesort.go
@@ -1,79 +1,11 @@
 package main
 
 import (
-	"flag"
-	"log"
 	"math"
-	"math/rand"
-	"os"
 	"runtime"
-	"runtime/pprof"
 	"sort"
-	"strconv"
 	"sync"
-	"time"
 )
-
-var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
-var memprofile = flag.String("memprofile", "", "write mem profile to file")
-var num = flag.String("num", "8", "number of nodes")
-
-func ffprepare(src []int64) {
-	rand.Seed(time.Now().Unix())
-	for i := range src {
-		src[i] = rand.Int63()
-	}
-}
-
-func main() {
-	flag.Parse()
-	// ==================== CPUProfile ======================
-	if *cpuprofile != "" {
-		f, err := os.Create(*cpuprofile)
-		if err != nil {
-			log.Fatal("could not create CPU profile: ", err)
-		}
-		defer f.Close()
-		if err := pprof.StartCPUProfile(f); err != nil {
-			log.Fatal("could not start CPU profile: ", err)
-		}
-		defer pprof.StopCPUProfile()
-	}
-
-	// =========== count number of goroutine running every 100s ===============
-	if *num != "" {
-		_, err := strconv.Atoi(*num)
-		if err != nil {
-			log.Fatal(err)
-		}
-		// nCPU := n
-	}
-
-	// ======== running code here ============
-	lens := []int{1, 3, 5, 7, 11, 13, 17, 19, 23, 29, 1024, 1 << 13, 1 << 17, 1 << 19, 1 << 20}
-
-	for i := range lens {
-		src := make([]int64, lens[i])
-		expect := make([]int64, lens[i])
-		ffprepare(src)
-		copy(expect, src)
-		MergeSort(src)
-	}
-
-	// =========== MemProfile ============
-	if *memprofile != "" {
-		f, err := os.Create(*memprofile)
-		if err != nil {
-			log.Fatal("could not create memory profile: ", err)
-		}
-		defer f.Close()
-		runtime.GC() // get up-to-date statistics
-		if err := pprof.WriteHeapProfile(f); err != nil {
-			log.Fatal("could not write memory profile: ", err)
-		}
-	}
-
-}
 
 // MergeSort performs the merge sort algorithm.
 // Please supplement this function to accomplish the home work.
